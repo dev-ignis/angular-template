1d2303a57b7d40d0a2347969fc1b7a5a1f300d6e
ignis <maildevcom+1@gmail.com>
2024-02-06 20:54:16 -0500

    feat: - enable POST caching;

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,15 @@ import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideClientHydration, withHttpTransferCacheOption } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideServiceWorker('ngsw-worker.js', {
+  providers: [provideRouter(routes), provideClientHydration(
+    withHttpTransferCacheOption({
+      includePostRequests: true,
+    })),
+    provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
     })]
